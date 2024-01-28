039cb352a6234a404d37d3f4d550d6851d38764a
Aliyu Abdulrazak <106888664+Razak002@users.noreply.github.com>
2024-01-29 00:41:24 +0100

    frequent questions page update

--- src/app/componenets/Cta.js
+++ src/app/componenets/Cta.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Cta = () => {
   return (
@@ -11,6 +12,17 @@ const Cta = () => {
           <p className='text-[0.7rem] md:text-sm p-4'>Dive into our Gallery and Own a Piece of Captivating Beauty.Discover More, Collect Now!
             Dive into our Gallery and Own a Piece of Captivating Beauty.Discover More, Collect Now!
           </p>
+
+          <ul className="flex justify-center items-center gap-6 text-sm">
+            <li>
+              <Link
+                href="/contact"
+                className="block rounded-md bg-white px-5 py-2.5 text-sm font-semibold text-green-800 transition "
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
 
         </div>
       </section>
--- src/app/componenets/Footer.js
+++ src/app/componenets/Footer.js
@@ -1,8 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
+import images from './ImageCon'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex justify-between items-center bg-green-800 p-4 '>
+
+      <div>
+        <Image src={images.gallery} alt='' />
+      </div>
+
+      <div className='text-white'>
+        <h2>contact us</h2>
+      </div>
+
+    </div>
   )
 }
 
