b642c31e177728a0afdd64a30dd2db493c2f8996
Aliyu Abdulrazak <106888664+Razak002@users.noreply.github.com>
2024-01-30 21:12:13 +0100

    hero text updated

--- src/app/componenets/Hero.js
+++ src/app/componenets/Hero.js
@@ -1,19 +1,17 @@
 import Image from 'next/image'
 import React from 'react'
-import images from './ImageCon'
 import Link from 'next/link'
 
 
-const Hero = ({data}) => {
+const Hero = ({ data }) => {
   return (
     <div className='max-w-screen-2xl px-4 sm:px-6 lg:px-8 py-16 flex flex-col md:flex-row justify-between items-center  '>
       <div className='p-4 space-y-4'>
         <h1 className='text-2xl font-bold'>
           {data?.heading}
         </h1>
-        <h2 className='text-sm font-semibold text-green-800'>Where Every Click Tells a Tale, Every Frame Narrates an Epic Saga</h2>
-        <p className='text-sm font-normal '> we don't just capture moments; we transform them into epic stories. Our lens is the storyteller, and each click is a chapter that unfolds the magic of life. 
-          </p>
+        <h2 className='text-sm font-semibold text-green-800'>{data?.semiHead}</h2>
+        <p className='text-sm font-normal '>{data?.heroText}</p>
         <button className='py-4 sm:py-6 lg:py-8 ' >
           <Link
             href="/contact"
@@ -26,8 +24,13 @@ const Hero = ({data}) => {
       </div>
 
       <div>
-
-        <Image src={images.camera} alt='' width={600}  />
+        <Image
+          src={data?.heroImage?.url}
+          alt='hero image'
+          width={600}
+          height={300}
+          className='bg-green-800 '
+        />
 
       </div>
     </div>
